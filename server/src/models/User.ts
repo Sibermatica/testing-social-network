6588a608d22eb9e405424f7faa720d995b6932ee
Yisus <jesusgameroom@gmail.com>
2024-01-05 14:29:09 -0400

    database model

---
+++ server/src/models/User.ts
@@ -0,0 +1,17 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+export default model("User", userSchema);
