b526212600898a27917b107262b2fb6ac491fcd5
Yisus <jesusgameroom@gmail.com>
2024-01-05 15:14:58 -0400

    hashing function

--- server/src/routes/index.routes.ts
+++ server/src/routes/index.routes.ts
@@ -1,8 +1,21 @@
-import { Router } from "express";
+import { Router } from 'express';
 const router = Router();
 
 router.get('/api', (_, res) => {
-    res.json({ message: "TSN - API" });
+    res.status(200).json({ message: 'TSN - API' });
+});
+
+router.post('/api/user', (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        res.status(400).json({
+            message: 'Username or password missing.',
+        });
+        return;
+    }
+
+    // TODO
 });
 
 export default router;
