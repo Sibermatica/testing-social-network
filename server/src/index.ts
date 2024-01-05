85def87fcd673b50fea4550d6739c16833151b3f
Yisus <jesusgameroom@gmail.com>
2024-01-05 12:54:33 -0400

    basic express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hi mom!");
+import express from 'express';
+const app = express();
+
+// settings
+app.set('port', process.env.PORT || 3000);
+
+// middlewares
+app.use((req, _, next) => {
+    console.log(`${req.url} -${req.method}`);
+
+    next();
+});
+
+// routes
+app.get('/', (_, res) => {
+    res.json({message: "Testing"});
+});
+
+// listen
+app.listen(app.get('port'), () => {
+    console.log(`Server runing: ${app.get('port')}`);
+});
